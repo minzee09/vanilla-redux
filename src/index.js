5171497f70c72fdfbcc605ecda90f26fbad10450
Minji Kim <minjikim2000@hotmail.com>
2024-04-14 22:42:37 +0900

    add vanilla redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 };
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
+countStore.subscribe(onChange);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
