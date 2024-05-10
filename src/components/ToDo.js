62e7fb7307fc35fba03002eaea3f36d451619d29
Minji Kim <minjikim2000@hotmail.com>
2024-05-10 20:00:30 +0900

    feat: add link to detail page

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreaters } from "../store";
+import { Link } from "react-router-dom";
 
-const ToDo = ({ text, onBtnClick }) => {
+const ToDo = ({ text, onBtnClick, id }) => {
   return (
     <li>
+      <Link to={`/${id}`} />
       {text}
       <button onClick={onBtnClick}>DEL</button>
     </li>
