a57451d699abab5b257e97c7a88d41539413e67d
Minji Kim <minjikim2000@hotmail.com>
2024-05-24 20:58:16 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -10,14 +10,26 @@ console.log("addToDo()") : object
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      const newToDo = { text: action.payload, id: Date.now() };
-      const updatedState = [...state, newToDo];
-      localStorage.setItem("toDos", JSON.stringify(updatedState));
-      return updatedState;
-    case deleteToDo.type:
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       const newToDo = { text: action.payload, id: Date.now() };
+//       const updatedState = [...state, newToDo];
+//       localStorage.setItem("toDos", JSON.stringify(updatedState));
+//       return updatedState;
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ id: Date.now(), text: action.payload });
+    })
+    .addCase(deleteToDo, (state, action) => {
       return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
